d79f71939f1c707d48ad141e06bbd2349a8d3761
LuizFMoreira <bluizfernando720@gmail.com>
2026-03-31 00:23:34 -0300

    adiciona rota GET para listar categorias

--- src/modules/categories/categories.controller.ts
+++ src/modules/categories/categories.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Get, Body } from '@nestjs/common'; // Adicione o Get aqui no import!
 import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 
@@ -6,10 +6,14 @@ import { CreateCategoryDto } from './dto/create-category.dto';
 export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
 
-  // O @Post indica que essa rota serve para CRIAR algo novo (receber dados)
   @Post()
   create(@Body() createCategoryDto: CreateCategoryDto) {
-    // Repassamos o que recebemos no @Body para o nosso Service trabalhar
     return this.categoriesService.create(createCategoryDto);
+  }
+
+  // NOVA ROTA: Quando alguém fizer um GET em /categories, ele cai aqui
+  @Get()
+  findAll() {
+    return this.categoriesService.findAll();
   }
 }
--- src/modules/categories/categories.service.ts
+++ src/modules/categories/categories.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { SupabaseService } from '../../supabase/supabase.service';
+import { CreateCategoryDto } from './dto/create-category.dto';
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+  constructor(private readonly supabase: SupabaseService) {}
+
+  // Nosso POST usa essa aqui:
+  async create(createCategoryDto: CreateCategoryDto) {
+    const { data, error } = await this.supabase.getClient()
+      .from('categories')
+      .insert([createCategoryDto])
+      .select();
+
+    if (error) throw new InternalServerErrorException(error.message);
+    return data;
+  }
+
+  // NOVA FUNÇÃO: Nosso GET vai usar essa aqui!
+  async findAll() {
+    // Aqui usamos o .select() vazio para dizer: "Traga TUDO dessa tabela"
+    const { data, error } = await this.supabase.getClient()
+      .from('categories')
+      .select('*'); 
+
+    if (error) throw new InternalServerErrorException(error.message);
+    return data;
+  }
+}
